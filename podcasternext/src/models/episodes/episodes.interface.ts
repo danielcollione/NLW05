d7900dbbfa23261b1dd2c0e56f01a1a522b470ef
danielcollione <daniel.collione22@gmail.com>
2021-04-23 16:51:03 -0300

    'push'

---
+++ podcasternext/src/models/episodes/episodes.interface.ts
@@ -0,0 +1,12 @@
+export type EpisodeType = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: string;
+  durationAsString: string;
+  url: string;
+  description?: string;
+  //....
+};
