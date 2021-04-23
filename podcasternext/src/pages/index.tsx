182e63c3c78588f2730460d223423fb1c9cb009f
danielcollione <daniel.collione22@gmail.com>
2021-04-23 09:33:26 -0300

    'aula2'

--- podcasternext/src/pages/index.tsx
+++ podcasternext/src/pages/index.tsx
@@ -1,7 +1,25 @@
-import { Header } from "../components/Header";
+// SPA - Funciona Apenas com o JS do Navegador Habilitado
+// SSR - Redenrização dos dados no servidor
+// SSG - Renderização dos dados no servidor, porém com controle de tempo (Versão estática)
 
-export default function Home() {
+import { useEffect } from "react"
+
+export default function Home(props) {
+  console.log(props.episodes);
+
   return (
-    <div></div>
+    <div>INDEX</div>
   )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
